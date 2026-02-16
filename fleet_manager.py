e050f8729b430b6c9838ae2f28c11270aeb40eb4
Shivani1537 <sj10@kent.ac.uk>
2026-02-16 08:00:16 +0000

    fleet_manager 1

---
+++ fleet_manager.py
@@ -0,0 +1,16 @@
+def init_database():
+
+    names = ["Jones", "Smith", "Matthews"]
+    ranks = ["Captain", "Commander", "lieutenant"]
+    divisions = ["Command", "Operations", "Safety"]
+    crew_ids = ["1", "2", "3"]
+
+    return names, ranks, divisions, crew_ids
+
+names, ranks, divisions, crew_ids = init_database()
+
+print(names)
+print(ranks)
+print(divisions)
+print(crew_ids)
+
