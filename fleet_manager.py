403b400d90d012c180b79b6d3a9782f4c8827148
Shivani1537 <sj10@kent.ac.uk>
2026-02-17 04:55:42 +0000

    fleet_manager 2

--- fleet_manager.py
+++ fleet_manager.py
@@ -14,3 +14,20 @@ print(ranks)
 print(divisions)
 print(crew_ids)
 
+def display_menu(student_name):
+    print("\n--- Fleet Manager ---")
+    print("Logged in as:", student_name)
+    print("1 - Display crew")
+    print("2 - Add member")
+    print("3 - Remove member")
+    print("4 - Update rank")
+    print("5 - Search Crew")
+    print("6 - Filter by division")
+    print("7 - Calculate payroll")
+    print("8 - Count Senior Officers")
+    print("9 - Exit")
+
+    choice = input("Choose option: ")
+    return choice
+
+display_menu("Shivani")
